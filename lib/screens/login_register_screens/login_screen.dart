b989343d64ea9b9daa3c000740b7329af95588aa
Saeed Noori <noori.saeed1375@gmail.com>
2022-10-18 20:44:04 +0330

    Change and Create some  directories

--- lib/screens/login_register_screens/login_screen.dart
+++ lib/screens/login_register_screens/login_screen.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:shop_app/config/default_colors.dart';
-import 'package:shop_app/screen/login_register_screens/components/button_component.dart';
-import 'package:shop_app/screen/login_register_screens/components/social_account_option.dart';
-import 'package:shop_app/screen/login_register_screens/components/textfield_component.dart';
+import 'package:shop_app/screens/login_register_screens/components/button_component.dart';
+import 'package:shop_app/screens/login_register_screens/components/social_account_option.dart';
+import 'package:shop_app/screens/login_register_screens/components/textfield_component.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
